ae2a0b88b2ecda4fd3271977960286f44999308b
Samueljain26 <samueljain77@gmail.com>
2025-04-01 15:07:51 +0530

    [Samuel] ADD:extracting capital words from string.

---
+++ src/main/java/com/bridgelabz/regex/CapitalizedWords.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CapitalizedWords {
+    public static void main(String[] args) {
+            String regex = "[A-Z]+[a-zA-Z0-9]{0,}";
+            Pattern pattern = Pattern.compile(regex);
+
+            String input = "My M Name is samuel";
+            Matcher matcher = pattern.matcher(input);
+
+            while (matcher.find()) {
+                System.out.println(matcher.group());
+            }
+        }
+    }

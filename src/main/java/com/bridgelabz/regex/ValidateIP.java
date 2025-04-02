ca288d3f9897b6948f7dbf44517c10a5f19e59c9
Samueljain26 <samueljain77@gmail.com>
2025-04-02 10:16:12 +0530

    [Samuel] ADD:Validating IP address.

---
+++ src/main/java/com/bridgelabz/regex/ValidateIP.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.regex;
+
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class ValidateIP {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        String regex="^(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2}).+(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2}).+(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2}).+(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})$";
+        Pattern pattern = Pattern.compile(regex);
+        String input = sc.nextLine();
+        Matcher matcher = pattern.matcher(input);
+
+        System.out.println(input + " is valid: " + matcher.matches());
+
+    }
+}
+/*1.0.0.0
+1.0.0.0 is valid: true
+ */
